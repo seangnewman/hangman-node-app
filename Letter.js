a1760af4de0e77408a3910f570933b11f76319ea
Sean Newman <seangnewman@msn.com>
2018-04-11 19:30:01 -0700

    Clean up for Letter module

--- Letter.js
+++ Letter.js
@@ -1,42 +1,19 @@
-// Will user prompt for this excercise
-// npm i prompt
-var prompt = require('prompt');
-var colors= require("colors/safe");
-
+// Letter constructor 
 var Letter = function(character){
-    
-    this._character = character;
+    this._character = character;  //Initialize object with character
     this._guessed;
-    this._isGuessed = function(){
+    this._isGuessed = function(){ 
         var placeHolder = '_';
         return this._guessed === true? this._character : placeHolder;
     };
     this._evaluateGuess = function(theCharacter){
+        //Set guessed flag 
         this._guessed = theCharacter === this._character? true: false;
-        console.log("Is it true?", this._guessed);
     }
 };
-
-var promptObj = {
-    name: 'characterGuess',
-    description : colors.magenta('Guess a letter!'),
-    required : true
-};
+ 
+module.exports = Letter;
 
 
-var test = new Letter('a');
-
-var testchar;
-prompt.start();
-prompt.get(promptObj, function(err, result){
-    //console.log(result.characterGuess);
-    testchar = result.characterGuess;
-    test._evaluateGuess(testchar);
-     console.log(test._isGuessed());
-
-});
-
-//var test = Letter('a');
-
-
+ 
 
