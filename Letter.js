9a36e29417a8804e2a24c82a4b79b6da8a718a2f
Sean Newman <seangnewman@msn.com>
2018-04-12 19:50:46 -0700

    Completed word module, cleaned up code

--- Letter.js
+++ Letter.js
@@ -1,15 +1,22 @@
-// Letter constructor 
+/**************************************************************************
+//Letter Constructor, base object (not class, no inheritance in js)
+***************************************************************************/
 var Letter = function(character){
-    this._character = character;  //Initialize object with character
-    this._guessed;
-    this._isGuessed = function(){ 
-        var placeHolder = '_';
-        return this._guessed === true? this._character : placeHolder;
-    };
-    this._evaluateGuess = function(theCharacter){
-        //Set guessed flag 
-        this._guessed = theCharacter === this._character? true: false;
-    }
+  this._character = character;  //Initialize object with character
+  //Initialize value to false
+  this._guessed = false;
+    
+  //return char if the guess was correct, otherwise a blank placeholder
+  this._isGuessed = function(){ 
+    var placeHolder = ' _ ';
+    return this._guessed === true? ' ' + this._character + ' ' : placeHolder;
+  }; // End 
+
+  //return if the character was guessed
+  this._evaluateGuess = function(theCharacter){
+  //Set guessed flag 
+    this._guessed = theCharacter === this._character? true: false;
+   }
 };
  
 module.exports = Letter;
--- Word.js
+++ Word.js
@@ -1,2 +1,40 @@
 // Dependent upon the Letter modue
-var Letter = require('/Letter.js');
+var Letter = require('./Letter.js');
+
+/**************************************************************************
+//Word Constructor, utilizes "has-a" relationship with Letter
+***************************************************************************/
+var Word = function(word){
+  //Create an array of characters
+  this._word = word.split('');
+
+  // Create an array of letter objects
+  this._arrayOfCharacters=[];
+    
+  //returns the string to indicate to identify which letters have been selected
+  this._currentString = function(){
+    if( this._arrayOfCharacters.length === 0){
+    // Initializes the array
+      for (var i=0; i < this._word.length; i++){
+        this._arrayOfCharacters.push(new Letter(this._word[i]));
+      }
+    }
+
+    var newCharacterString = [];
+    for(var i=0; i < this._arrayOfCharacters.length; i++){
+      newCharacterString.push(this._arrayOfCharacters[i]._isGuessed());
+    }
+ 
+    return newCharacterString.join('');
+  };  // End return string
+  
+  
+  this._guessCharacter = function(character){
+  //given a character, call 
+    for(var i=0; i <this._arrayOfCharacters.length; i++){
+      this._arrayOfCharacters[i]._guessed === true? this._arrayOfCharacters[i]._isGuessed() :this._arrayOfCharacters[i]._evaluateGuess(character) ;
+    }
+  }; // End guess character
+}
+
+module.exports = Word;
